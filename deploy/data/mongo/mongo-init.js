f16e0c2140a13451b58d8ef3b1bfc9410d1c64eb
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-05-23 17:56:33 +0300

    initial commit

--- deploy/data/mongo/mongo-init.js
+++ deploy/data/mongo/mongo-init.js
@@ -1,12 +1,24 @@
 db.createUser(
     {
-        user: "notification_admin",
-        pwd: "qwerty123",
-        roles: [
-            {
-                role: "readWrite",
-                db: "notifications"
-            }
-        ]
+        user: "mongo-root",
+        pwd: "password",
+        roles: [ { role: "root", db: "admin" } ]
     }
 );
+
+db.createUser(
+    {
+        user: "mongo-admin",
+        pwd: "password",
+        roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+    }
+);
+
+db.createUser(
+    {
+        user: "notification-admin",
+        pwd: "password",
+        roles: [ { role: "readWrite", db: "notifications" } ]
+    }
+);
+
