f02d935905c83f3f2320cc3ee5d31398731493e2
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-10-26 19:08:07 +0300

    initial commit

---
+++ home_notes/src/main/java/ru/home/notes/HomeNotesApplication.java
@@ -0,0 +1,15 @@
+package ru.home.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeNotesApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HomeNotesApplication.class, args);
+	}
+
+}
---
+++ home_notes/src/test/java/ru/home/notes/HomeNotesApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.home.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeNotesApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
