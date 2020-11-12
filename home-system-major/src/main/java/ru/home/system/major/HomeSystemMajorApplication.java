30f9e960ccf88becde94aa7febfb813741b41c7f
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-11-12 23:07:24 +0300

    first commit

---
+++ home-system-major/src/main/java/ru/home/system/major/HomeSystemMajorApplication.java
@@ -0,0 +1,15 @@
+package ru.home.system.major;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSystemMajorApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HomeSystemMajorApplication.class, args);
+	}
+
+}
---
+++ home-system-major/src/test/java/ru/home/system/major/HomeSystemMajorApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.home.system.major;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSystemMajorApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
