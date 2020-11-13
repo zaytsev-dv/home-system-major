7486f1ca913f0155392017efe6be77536eea3fdb
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-11-13 04:11:40 +0300

    remove unused codes

--- core/src/main/java/ru/home/system/major/core/adapters/telegram/KeyboardCreator.java
+++ core/src/main/java/ru/home/system/major/core/adapters/telegram/KeyboardCreator.java
@@ -1,10 +1,7 @@
 package ru.home.system.major.core.adapters.telegram;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ForceReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import ru.home.system.major.core.dto.TelegramButtonCallbackData;
 
 import java.util.ArrayList;
@@ -13,7 +10,8 @@ import java.util.List;
 
 public class KeyboardCreator
 {
-	static InlineKeyboardMarkup getKeyboard() {
+	static InlineKeyboardMarkup getKeyboard()
+	{
 		InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 		InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
 		inlineKeyboardButton.setText("Тык");
@@ -23,7 +21,8 @@ public class KeyboardCreator
 		return inlineKeyboardMarkup;
 	}
 
-	static InlineKeyboardMarkup getKeyboard(List<TelegramButtonCallbackData> buttonNames) {
+	static InlineKeyboardMarkup getKeyboard(List<TelegramButtonCallbackData> buttonNames)
+	{
 		InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
 		List<InlineKeyboardButton> buttons = new ArrayList<>();
@@ -37,21 +36,5 @@ public class KeyboardCreator
 		inlineKeyboardMarkup.setKeyboard(Arrays.asList(buttons));
 
 		return inlineKeyboardMarkup;
-	}
-
-	static ForceReplyKeyboard getForceReplyKeyboard() {
-		ForceReplyKeyboard keyboardMarkup = new ForceReplyKeyboard();
-		keyboardMarkup.setForceReply(true);
-		return keyboardMarkup;
-	}
-
-	static ReplyKeyboardMarkup getReplyKeyboardMarkup() {
-		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-//		keyboardMarkup.setOneTimeKeyboard(true);
-		keyboardMarkup.setOneTimeKeyboard(false);
-		KeyboardRow keyboardButtons = new KeyboardRow();
-		keyboardButtons.set(1, "TETETETTETE");
-		keyboardMarkup.setKeyboard(Arrays.asList(keyboardButtons));
-		return keyboardMarkup;
 	}
 }
