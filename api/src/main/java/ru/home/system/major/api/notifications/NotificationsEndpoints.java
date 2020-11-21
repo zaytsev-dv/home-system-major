083f142169910e386137c5f712c3708660c08261
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-11-22 02:42:18 +0300

    add jaeger

--- api/src/main/java/ru/home/system/major/api/notifications/NotificationsEndpoints.java
+++ api/src/main/java/ru/home/system/major/api/notifications/NotificationsEndpoints.java
@@ -1,6 +1,10 @@
 package ru.home.system.major.api.notifications;
 
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.home.system.artifactory.annotations.ParamConstraint;
@@ -47,12 +51,33 @@ public class NotificationsEndpoints
 		return TryCatchService.runReturned(() -> id);
 	}
 
+	@Autowired
+	private Tracer tracer;
+
 	@GetMapping("/test/2")
 	public String test2(
 			@Valid
 			@ParamConstraint(conditionType = "regex", message = "error")
 			@RequestParam String id)
 	{
-		return TryCatchService.runReturned(() -> id);
+		String result = null;
+		Span span = tracer.buildSpan("test2")
+				.withTag("id", id)
+				.start();
+		try (Scope ignore = tracer.activateSpan(span))
+		{
+
+			result = TryCatchService.runReturned(() -> id);
+		}
+		catch (Exception e)
+		{
+			span.log(e.getMessage());
+		}
+		finally
+		{
+			span.finish();
+		}
+
+		return result;
 	}
 }
